fbd4f34968cc78a537d13def2f76217f3414f923
LanDev01 <alnjr0001@outlook.com>
2024-11-22 18:30:58 -0300

    Feat: criando component button

---
+++ src/components/button/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 56,
+    backgroundColor: "#8257E5",
+    borderRadius: 5,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    color: "#FFF",
+    fontSize: 16,
+  },
+});
